c05651300bd3c3012bd07f51acd8c7e5a22917b1
D. W <owl.robot@aol.com>
2021-08-24 22:10:42 -0400

    Update mergefeed.py

--- mergefeed.py
+++ mergefeed.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # Python 3.9.5
 
-'''Getting all the news headlines, with their links for the start of the day using
-Mergefeed.net. The one below has the headlines from Hackernews, TechCrunch, and Techmeme.
+'''Getting all the news headlines, with their links using Mergefeed.net. 
+The one below has the headlines from Hackernews, TechCrunch, and Techmeme.
 
 You can personalize it to the sites you prefer by going to Mergefeed.net,
-put the website's you like to follow, click the "Create Multifeed" button, and from there
+put the places you like to follow, click the "Create Multifeed" button, and from there
 copy and paste the address from the address bar and replace the url below'''
 
 import webbrowser
